36e89ee782bbdb91a7ace41e7cabe6b5ce51ba00
GovindNiranjanNarwade <govindpurva3@gmail.com>
2022-05-24 11:26:51 +0530

    somechanges

--- controller/office_controller.js
+++ controller/office_controller.js
@@ -7,7 +7,7 @@ exports.createOfficeAdmin = async(req,res)=>{
             Email:req.body.Email,
             Role:req.body.Role,
             Mobile:req.body.Mobile,
-            Password:bcrypt.hashSync(req.body.Password,10),
+            Password:req.body.Password,
             Address:req.body.Address,
             City:req.body.City,
             State:req.body.State,
